e29abd84fc9c49e87cbf7fd944fe266c19272d98
Larry Campos <larryisaza70@gmail.com>
2022-06-23 16:53:39 -0500

    Agregando funcionalidad al boton submit, cuando los inputs esten correctamente diligenciados y cuando no

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import * as eventsModule from './events.js';
 
 const formulary = document.getElementById("formulary");
 const inputs = document.querySelectorAll("input");
+const submit = document.getElementById("buttonSubmtit");
 
 formulary.addEventListener("submit", e =>{
     e.preventDefault();
@@ -11,3 +12,5 @@ inputs.forEach(input =>{
     input.addEventListener("keyup", eventsModule.validateDate);
     input.addEventListener("keydown", eventsModule.validateDate);
 });
+
+submit.addEventListener("click",eventsModule.validateSubmit);
